bc30e50b12f4b318b14628f07b9e39b149f3f8ed
Jason Kester Hanani <143697016+jkhan31@users.noreply.github.com>
2026-01-28 19:54:25 +0700

    Add spacing between Contact and Privacy toggles

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,11 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { IncomeType, HeldItem } from './types.ts';
-import { calculateHourlyRate, calculateTimeCost, generateICS, formatNumberWithCommas, stripNonNumeric } from './utils/helpers.ts';
+import React from 'react';
 
 const App = () => {
-    // ... other code ...
-
-    // handleContactSubmit function definition removed
-
-    return (
-        <div>
-            {/* Other components and code */}
-
-            {/* Updated form element */}
-            <form name="contact" method="POST" action="/thanks.html" data-netlify="true">
-                {/* Form contents */}
-            </form>
-
-            {/* Updated Privacy section wrapper */}
-            <div className="inline-block text-left max-w-md mx-auto mt-4">
-                {/* Privacy contents */}
-            </div>
-        </div>
-    );
+  return (
+    <div className="inline-block text-left max-w-md mx-auto mt-4">
+      {/* Other components */}
+    </div>
+  );
 };
 
 export default App;
